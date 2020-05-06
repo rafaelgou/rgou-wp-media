fa77c0acc164fbdad8c4676bc85b424a63e4bc0f
Rafael Goulart <rafaelgou@rgou.net>
2020-05-06 14:32:37 -0300

    Implement Crawler and tests

--- Tests/Unit/Rgou/WPMedia/classes/CrawlerTest.php
+++ Tests/Unit/Rgou/WPMedia/classes/CrawlerTest.php
@@ -3,29 +3,111 @@ namespace Rgou\WPMedia\Tests\Unit\classes;
 
 use Rgou\WPMedia\Tests\Unit\TestCase;
 use Rgou\WPMedia\Crawler;
+use DOMDocument;
 
 class CrawlerTest extends TestCase
 {
-    public function test_get_page_content()
+    public function test_get_content()
     {
 		$crawler = new Crawler('https://www.duckduckgo.com');
+		$content = $crawler->get_content();
 
-
-        $this->assertTrue(false);
+		$this->assertTrue(strlen( $content ) > 0);
     }
 
-    public function test_get_links()
+    public function test_get_dom_document()
     {
-        $this->assertTrue(false);
+		$crawler     = new Crawler('https://www.duckduckgo.com');
+		$htmlContent = $crawler->get_content();
+		$doc         = $crawler->get_dom_document( $htmlContent );
+
+        $this->assertTrue($doc instanceof DOMDocument);
     }
 
-    public function test_get_sitemap()
+	public function htmlProvider()
     {
-        $this->assertTrue(false);
+        return [
+            [
+				'<hmtl><body></body></html>',
+				0,
+				[]
+			],
+            [
+				'<hmtl><body>'
+				.'<a href="https://duckduckgo.com">DuckDuckGo</a>'
+				.'<a href="https://me.rgou.net">RGOU</a>'
+				.'<a href="https://tech.rgou.net">RGOU Tech Blog</a>'
+				.'</body></html>',
+				3,
+				[
+					[
+						"href"  => 'https://duckduckgo.com',
+						"label" => 'DuckDuckGo'
+					],
+					[
+						"href"  => 'https://me.rgou.net',
+						"label" => 'RGOU'
+					],
+					[
+						"href"  => 'https://tech.rgou.net',
+						"label" => 'RGOU Tech Blog'
+					],
+				]
+			],
+        ];
     }
 
-    public function test_run()
+    /**
+     * @dataProvider htmlProvider
+     */
+	public function test_get_links( $htmlContent, $count, $expectedLinks )
     {
-        $this->assertTrue(false);
+		$crawler = new Crawler('');
+		$doc     = $crawler->get_dom_document( $htmlContent );
+		$links   = $crawler->get_links( $doc );
+
+        $this->assertSame( count( $links) , $count );
+
+		foreach ($expectedLinks as $key => $expectedLink) {
+			$this->assertSame(
+				$expectedLink['href'],
+				$links[ $key ]['href']
+			);
+			$this->assertSame(
+				$expectedLink['label'],
+				$links[ $key ]['label']
+			);
+		}
+    }
+
+    /**
+     * @dataProvider htmlProvider
+     */
+    public function test_get_sitemap( $htmlContent, $count, $expectedLinks )
+    {
+		$crawler     = new Crawler('');
+		$doc         = $crawler->get_dom_document( $htmlContent );
+		$links       = $crawler->get_links( $doc );
+		$sitemap     = $crawler->get_sitemap( $links );
+
+		if ( $count === 0 ) {
+			$this->assertNull( $sitemap );
+		} else {
+			$docSitemap  = $crawler->get_dom_document( $sitemap );
+			$nodes = $docSitemap->getElementsByTagName('a');
+
+			$this->assertSame( count( $nodes) , $count );
+
+			foreach ($nodes as $key => $node) {
+				$this->assertSame(
+					$node->getAttribute('href'),
+					$expectedLinks[ $key ]['href']
+				);
+				$this->assertSame(
+					$node->nodeValue,
+					$expectedLinks[ $key ]['label']
+				);
+			}
+		}
     }
 }
--- classes/Rgou/WPMedia/Crawler.php
+++ classes/Rgou/WPMedia/Crawler.php
@@ -1,6 +1,9 @@
 <?php
 namespace Rgou\WPMedia;
 
+use DOMDocument;
+use DOMXpath;
+
 /**
  * The public-facing functionality of the plugin.
  *
@@ -23,42 +26,99 @@ class Crawler {
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $url    The URL to be crawled
+	 * @var      string $url The URL to be crawled
 	 */
 	private $url;
 
 	/**
-	 * Initialize the class and set its properties.
+	 * DEOM Document
+	 *
+	 * @since  0.1.0
+	 * @access private
+	 * @var DomDocument $doc The DOM document
+	 */
+	private $doc;
+
+	/**
+	 * The URL content
 	 *
 	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $content The URL content
+	 */
+	private $content;
+
+	/**
+	 * The Sitemap content
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $sitemap The Sitemap content
+	 */
+	private $sitemap;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since  1.0.0
 	 * @param  string $url The URL to be crawled.
 	 */
 	public function __construct( $url ) {
 
 		$this->url = $url;
-
 	}
 
 	/**
 	 * Get the page content.
 	 *
+	 * @since  1.0.0
 	 * @return string
 	 */
-	public function get_page_content() {
+	public function get_content() {
 
-		return '';
+		if ( null === $this->content ) {
+			// phpcs:disable
+			$this->content = file_get_contents( $this->url );
+		}
 
+		return $this->content;
 	}
+
+	/**
+     * Parse remote content into DomDocument
+	 *
+     * @param  string $htmlContent
+     * @return DOMDocument
+     */
+    public function get_dom_document( $htmlContent )
+    {
+
+        $doc = new DOMDocument;
+        libxml_use_internal_errors( true );
+		$doc->loadHTML( $htmlContent );
+
+        return $doc;
+    }
 
 	/**
 	 * Get the links.
 	 *
+	 * @param DOMDocument $doc Dom document
+	 *
 	 * @return links
 	 */
-	public function get_links() {
+	public function get_links(DOMDocument $doc) {
 
-		return array();
+		$nodes = $doc->getElementsByTagName('a');
+		$links = [];
+        foreach ($nodes as $node) {
+            $links[] = [
+				"href"  => $node->getAttribute('href'),
+				"label" => $node->nodeValue
+			];
+		}
 
+		return $links;
 	}
 
 	/**
@@ -66,10 +126,47 @@ class Crawler {
 	 *
 	 * @return string
 	 */
-	public function get_sitemap() {
+	public function get_sitemap( $links ) {
 
-		return '';
+		if ( ! is_array( $links ) ) {
+			return null;
+		}
 
+		if ( null === $this->sitemap ) {
+			$doc = new DOMDocument;
+			if ( count( $links ) === 0 ) {
+				return $this->sitemap;
+			}
+
+			$ul = $doc->createElement( 'ul' );
+			foreach( $links as $link ) {
+				$li = $doc->createElement( 'li' );
+				$a  = $doc->createElement( 'a', $link['label'] );
+				$a->setAttribute( 'href', $link['href'] );
+				$a->setAttribute( 'target', '_blank' );
+				$li->appendChild( $a );
+				$ul->appendChild( $li );
+			}
+			$body = $doc->createElement( 'body' );
+			$h1   = $doc->createElement( 'h1', 'Sitemap' );
+			$body->appendChild( $h1 );
+			$body->appendChild( $ul );
+
+			$css = $doc->createElement( 'link' );
+			$css->setAttribute( 'rel', 'stylesheet' );
+			$css->setAttribute( 'href', 'https://cdn.jsdelivr.net/npm/@ajusa/lit@latest/dist/lit.css' );
+
+			$head = $doc->createElement( 'head' );
+			$head->appendChild( $css );
+
+			$html = $doc->createElement( 'html' );
+			$html->appendChild( $head );
+			$html->appendChild( $body );
+
+			$this->sitemap = $doc->saveHTML( $html );
+		}
+
+		return $this->sitemap;
 	}
 
 	/**
